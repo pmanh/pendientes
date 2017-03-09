c27938d845ef004d37dafa8b4a8e16ad7c4a9fb2
ripper2hl <jesus.peralesmr@uanl.edu.mx>
2017-03-09 16:28:44 -0600

    reestructuracion de codigo

--- src/main/webapp/app/app/scripts/controllers/main.js
+++ src/main/webapp/app/app/scripts/controllers/main.js
@@ -9,55 +9,102 @@
 	 * Controller of the pendietesApp
 	 */
 	angular.module('FamsaPendings')
-	  .controller('MainCtrl', ['$scope','Pending','Type', MainCtrl]);
+	  .controller('MainCtrl', ['Pending','Type','UploadFile', MainCtrl]);
 	
 	/**
-	 * 
-	 * @param $scope
 	 * @param Pending
 	 * @param Type
 	 * @returns
 	 */
-	function MainCtrl($scope, Pending, Type) {
-		  var vm = this;
-		$scope.pendings =[];
-	    $scope.yeomanImg = ctxPath+"images/famsa.png";
-	    $scope.config = {
-	    	    itemsPerPage: 5,
-	    	    fillLastPage: true
-	    	  }
+	function MainCtrl(Pending, Type, UploadFile) {
+		var vm = this;
+		vm.pendings = [];
+	    vm.yeomanImg = ctxPath+"images/famsa.png";
+	    vm.config = {
+		    itemsPerPage: 5,
+		    fillLastPage: true
+		}
 	    
-	    $scope.loadTypes = loadTypes;
-	    $scope.loadPendings = loadPendings;
-
-	    $scope.addType = function(){
-	    	bootbox.prompt("Escriba el nuevo tipo de pendientes", function(result){    		
-	    		if(result && result.trim().length > 0){
-	    			Type.save({type:result}, function(resp){
-	    		    	$scope.loadTypes();
-	    		    });
-	    		}
-	    	});
+	    vm.loadTypes = loadTypes;
+	    vm.loadPendings = loadPendings;
+	    vm.addType = addType;
+	    vm.savePending = savePending;	    
+	    vm.deletePending = deletePending;
+	    
+	    vm.editPending = function(item){
+	    	if(item.idType && item.idType.id)
+	    		item.idType.id +='';
+	    	vm.pending = item;
+	    }
+	    console.debug(angular.identity)
+	    vm.submitfile =  function submitfile(){
+	    	UploadFile.save($("#uploadForm")).$promise.then(function(e){
+	    		console.debug(e);
+	    	})
+//	    	console.log('do')
+//	    	console.log($("#uploadForm"))
+//		    var formdata = new FormData($("#uploadForm"));
+//		    $.ajax({
+//		        type: "POST",
+//		        url: "http://localhost:8080/alfresco/service/api/upload?alf_ticket=TICKET_4f1b4dd7bdc76f254b2f534c1e00272d5193c493",
+//		        data: formdata,
+//		        processData: false,
+//		        contentType: false,
+//		        dataType: "json",
+//		        success: function(data, textStatus, jqXHR) {
+//		        	console.log(data)
+//		        },
+//		        error: function(data, textStatus, jqXHR) {
+//		        	console.log(data)
+//		           //process error msg
+//		        },
+//		    });
 	    }
 	    
-	    
-	    $scope.savePending = function(){
-	    	if($scope.pending.title && $scope.pending.title.trim().length >0){
-		    	if($scope.pending.id){
-		    		Pending.update($scope.pending).$promise.then(function(resp) {
-		    			$scope.pending = {};
-		    	    	$scope.loadPendings()
-		    		});
+	    /**
+	     * 
+	     */
+	    function loadTypes(){
+	    	console.debug('loadTypes -->')
+	    	Type.get().$promise.then((types) => vm.types = types.data,console.debug('loadTypes ==>'));
+	    	console.debug('loadTypes //>')
+	    }
+	   
+	   /**
+	    * 
+	    */
+	   function loadPendings(idType){
+	    	vm.filter = idType?{'idType.id':idType}:{};
+	    	Pending.get(vm.filter).$promise.then((resp)=>vm.pendings = resp.data);
+	    }	    
+	  }
+		/**
+		 * @returns
+		 */
+		function savePending(){
+	    	if(vm.pending.title && vm.pending.title.trim().length >0){
+		    	if(vm.pending.id){
+		    		Pending.update(vm.pending).$promise.then((resp) => vm.pending = {},vm.loadPendings());
 		    	}else{
-		    		Pending.save($scope.pending).$promise.then(function(resp) {
-		    			$scope.pending = {};
-		    	    	$scope.loadPendings()
-		    		});    		
+		    		Pending.save(vm.pending).$promise.then((resp)=> vm.pending = {}, vm.loadPendings());    		
 		    	}
 	    	}
 	    }
-	    
-	    $scope.deletePending = function(item){
+		/**
+		 * @returns
+		 */
+		function addType(){
+	    	bootbox.prompt("Escriba el nuevo tipo de pendientes", function(result){    		
+	    		if(result && result.trim().length > 0){
+	    			Type.save({type:result}, (resp)=> vm.loadTypes());
+	    		}
+	    	});
+	    }
+		/**
+		 * @param item
+		 * @returns
+		 */
+		function deletePending(item){
 	    	bootbox.confirm({
 	    	    message: "\u00bfEsta seguro que desea eliminar el registro?",
 	    	    buttons: {
@@ -72,65 +119,9 @@
 	    	    },
 	    	    callback: function (result) {
 	    	       if(result){
-		    	       	Pending.drop(item).$promise.then(function(resp) {
-		    	        	$scope.loadPendings()
-		    	    	});
+		    	       	Pending.drop(item).$promise.then((resp)=> vm.loadPendings());
 	    	       }
 	    	    }
 	    	});
-
 	    }
-	    
-	    $scope.editPending = function(item){
-	    	if(item.idType && item.idType.id)
-	    		item.idType.id +='';
-	    	$scope.pending = item;
-	    }
-	    
-	    $scope.submitfile =  function submitfile(){
-	    	console.log('do')
-	    	console.log($("#upload_form"))
-		    var formdata = new FormData($("#upload_form"));
-		    $.ajax({
-		        type: "POST",
-		        url: "http://localhost:8080/alfresco/service/api/upload?alf_ticket=TICKET_58f816fc90d18ed0aea284cbfed16ba8bbb087a2",
-		        data: formdata,
-		        processData: false,
-		        contentType: false,
-		        dataType: "json",
-		        success: function(data, textStatus, jqXHR) {
-		        	console.log(data)
-		           //process data
-		        },
-		        error: function(data, textStatus, jqXHR) {
-		        	console.log(data)
-		           //process error msg
-		        },
-		    });
-	    }
-	    
-	    /**
-	     * 
-	     */
-	    function loadTypes(){
-	    	console.debug('loadTypes -->')
-	    	Type.get({}).$promise.then(function(types) {
-	    		$scope.types = types.data;
-	    		console.debug('loadTypes ==>')
-	    	});
-	    	console.debug('loadTypes //>')
-	    }
-	   
-	   /**
-	    * 
-	    */
-	   function loadPendings(idType){
-	    	$scope.filter = {};
-	    	if(idType)
-	    		$scope.filter = {'idType.id':idType};
-	    	Pending.get($scope.filter).$promise.then(function(resp) {
-	    		$scope.pendings = resp.data;
-	    	});
-	    }	    
-	  }
 })();
--- src/main/webapp/app/app/scripts/services/services.js
+++ src/main/webapp/app/app/scripts/services/services.js
@@ -1,3 +1,5 @@
+(function(){
+'use strict';
 /**
  * @author Pedro Manuel Hernandez
  * @since 2017-01-31 
@@ -14,4 +16,23 @@ angular.module('FamsaPendings')
         update: {method:'PUT'},
         drop: {method: 'DELETE'}
       });
+})
+.factory('UploadFile', function ($resource) {
+    return $resource('http://localhost:8080/alfresco/service/api/upload?alf_ticket=TICKET_4f1b4dd7bdc76f254b2f534c1e00272d5193c493',{},{
+        save: { method:'PUT',
+	           	transformRequest: formDataObject,
+	            headers: {'Content-Type':undefined, enctype:'multipart/form-data'}
+	    }
+      });
 });
+
+
+function formDataObject (data) {
+    var fd = new FormData();
+    angular.forEach(data, function(value, key) {
+        fd.append(key, value);
+    });
+    return fd;
+}
+
+})();
